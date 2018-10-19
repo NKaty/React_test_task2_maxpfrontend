6254e1d7e7315dcb18e6498a519db94b5f93f991
NKaty <nd.katy@gmail.com>
2018-10-19 18:50:46 +0300

    Fixed error handling with LOCATION_CHANGE

--- src/reducer/error.js
+++ src/reducer/error.js
@@ -1,9 +1,10 @@
 import { RESET_ERROR_MESSAGE } from '../constants'
+import { LOCATION_CHANGE } from 'react-router-redux'
 
 export default (state = null, action) => {
   const { type, error } = action
 
-  if (type === RESET_ERROR_MESSAGE) {
+  if (type === RESET_ERROR_MESSAGE || type === LOCATION_CHANGE) {
     return null
   } else if (error) {
     return error
